5022e7abe9f4f3af037733c05d48a752dcc5a5a1
iip-tuananh <tuananhdinamo1122@gmai.com>
2026-01-13 08:33:37 +0700

    a

--- resources/views/product/list.blade.php
+++ resources/views/product/list.blade.php
@@ -36,6 +36,9 @@ Danh sách {{$title}}
    <div class="container ">
       <div class="bg_collection section">
          <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12">
+               <img src="{{url('frontend/images/image.jpg')}}" alt="bg_collection">
+            </div>
             <aside class="dqdt-sidebar sidebar left-content col-lg-3 col-md-12 col-sm-12">
                <div class="wrap_background_aside asidecollection">
                   <aside class="aside-item sidebar-category collection-category margin-bottom-20">
