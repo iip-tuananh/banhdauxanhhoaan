3523ebbc38c2ab388fa46d586257a622b1adbbcd
iip-tuananh <tuananhdinamo1122@gmai.com>
2026-01-15 08:38:50 +0700

    a

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -45,7 +45,7 @@
          <div class="container">
          <div class="row">
                <div class="col-lg-12 col-md-12 col-sm-12 text-center mb-3">
-                  <img style="max-width: 700px;" src="{{url('frontend/images/image.jpg')}}" alt="bg_collection">
+                  <img class="imagehome" src="{{url('frontend/images/image.jpg')}}" alt="bg_collection">
                </div>
             <div class="col-lg-12">
                <div class="section_title_base">
